400d58c87be3548b794c5fa9d86f4084e16e02bb
mcenno <enno.middelberg@posteo.de>
2021-10-05 07:52:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def hello():
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=8080)
     
